b124edc89992a862efa8f5a8dc6b088d48101caf
kalho <luisenriquez11@hotmail.com>
2022-01-16 19:06:33 -0500

    User prompt response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var type = {
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numeric: "0123456789",
-  specialCharaters: "!@#$%^&*()"
-};
+var chars ="";
+var passwordLength = 0;
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChar = "!@#$%^&*()";
+
 
 
 
 // Write password to the #password input
 function writePassword() {
   
-  generate();
+  userPrompt();
 
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  // passwordText.value = password;
+  var password = getPassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 
 }
 
@@ -25,17 +26,35 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generate() {
-  var passwordLength = window.prompt("Enter length of the password(from 8 to 128 characters):");
+function userPrompt() {
+  passwordLength = window.prompt("Enter length of the password(from 8 to 128 characters):");
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Length should be between 8 and 128 characters!");
-    generate();
+    userPrompt();
+    return;
   }
+
   var lowerCaseUse = window.confirm("Do you want letters in lowercase?");
-  console.log(lowerCaseUse);
   var upperCaseUse = window.confirm("Do you want letters in uppercase?");
   var numericUse = window.confirm("Do you want numbers?");
   var charactersUse = window.confirm("Do you want special characters?");
 
-
+  if (lowerCaseUse === true) {
+    chars = chars.concat(lowerCase);
+  }
+  if (upperCaseUse === true) {
+    chars = chars.concat(upperCase);
+  }
+  if (numericUse === true) {
+    chars = chars.concat(numbers);
+  }
+  if (charactersUse === true) {
+    chars = chars.concat(specialChar);
+  }
+  console.log(chars);
 }
+
+function getPassword() {
+  
+}
+
